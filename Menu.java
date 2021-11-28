8f8e42055c6aa653d8532c88d70a4b2a428a4a11
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-11-28 16:42:57 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
 public class Menu {
-public static Scanner leitor = new Scanner(System.in);//cria objeto scanner pra ler as entradas do usuario
+
+	public static Scanner leitor = new Scanner(System.in);
 	
 	public static Cadastro lerCadastro() {	
 		System.out.println("Cadastrando um usuario.");
 		System.out.println("Por favor, digite os dados pedidos.");
 		System.out.print("Digite o nome:");		
-		String nome = leitor.nextLine(); //le o nome
+		String nome = leitor.nextLine(); 
 		System.out.print("Digite o email:");		
-		String email = leitor.nextLine(); //le o email
+		String email = leitor.nextLine();
 		System.out.print("Digite o senha:");		
-		String senha1 = leitor.nextLine();  //le a senha
-		System.out.print("Digite a confirmacao de senha:");		
-		String senha2 = leitor.nextLine();
+		String senha = leitor.nextLine();
 		Cadastro cad = null;
-		if (senha1.equals(senha2)) {
-			cad = new Cadastro(nome, email, senha1);
+		if (senha.equals(cad.getSenha())) {
+			cad = new Cadastro(nome, email, senha);
 		}
-		return cad; //retorna o objeto cadastro
+		return cad;
 	}
 	
 	public static Usuario lerUsuario() {
 		System.out.println("Por favor, digite os dados pedidos.");
 		System.out.print("Digite o nome:");		
-		String nome = leitor.nextLine();//le o nome
+		String nome = leitor.nextLine();
 		System.out.print("Digite o email:");		
-		String email = leitor.nextLine(); //le o email
-		Usuario usu = new Usuario (nome, email); //cria um usuario no sistema com as informaçoes obtidas
-		return usu; //retorna o objeto usuario
+		String email = leitor.nextLine();
+		System.out.print("Digite o senha:");		
+		Usuario usu = new Usuario (nome, email); 
+		return usu; 
 	}
 }
