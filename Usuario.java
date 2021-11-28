f32883e7a5e585667ff8e6e403760db7f5f9e23f
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-11-28 16:43:42 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -3,23 +3,63 @@ import java.util.ArrayList;
 public class Usuario {
 	
 	public Usuario(String nome, String email) {
-		//construtor da classe com  nome e email
+		
 		this.nome = nome;
 		this.email = email;
 	}
-	//variaveis da classe
+	
 	public String nome;
 	public String email;
+	public String senha = "senha123";
 	
-	//arrays de objeto do tipo usuario e objeto do tipo filme
 	public ArrayList<Usuario> usuarios = new ArrayList<>();	
 	public ArrayList<Filme> filmes = new ArrayList<>();
 	
 	public void imprimir() {
-		//metodo para imprimir dados do usuario
+		
 		System.out.println("Dados do usuario");
 		System.out.println("Nome:"+nome);
 		System.out.println("Email:"+email);
 		System.out.println("Filmes locados: "+filmes.size());
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public ArrayList<Filme> getFilmes() {
+		return filmes;
+	}
+
+	public void setFilmes(ArrayList<Filme> filmes) {
+		this.filmes = filmes;
 	}
 }
