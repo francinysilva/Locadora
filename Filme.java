43a5459af895b8295571ea4da9a76a0d623223e1
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-11-28 16:43:23 -0300

    Update Filme.java

--- Filme.java
+++ Filme.java
@@ -1,8 +1,39 @@
 public class Filme {
-	//classe filme
+	
 	public String nomeFilme;
 	public int codFilme;
 	public double valorFilme;
 	public String descricaoFilme;
-	public boolean filmeLocado;	
+	public boolean filmeLocado;
+	
+	public String getNomeFilme() {
+		return nomeFilme;
+	}
+	public void setNomeFilme(String nomeFilme) {
+		this.nomeFilme = nomeFilme;
+	}
+	public int getCodFilme() {
+		return codFilme;
+	}
+	public void setCodFilme(int codFilme) {
+		this.codFilme = codFilme;
+	}
+	public double getValorFilme() {
+		return valorFilme;
+	}
+	public void setValorFilme(double valorFilme) {
+		this.valorFilme = valorFilme;
+	}
+	public String getDescricaoFilme() {
+		return descricaoFilme;
+	}
+	public void setDescricaoFilme(String descricaoFilme) {
+		this.descricaoFilme = descricaoFilme;
+	}
+	public boolean isFilmeLocado() {
+		return filmeLocado;
+	}
+	public void setFilmeLocado(boolean filmeLocado) {
+		this.filmeLocado = filmeLocado;
+	}
 }
