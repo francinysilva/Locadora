1446ae3ffc0df02967669cb68fe8e636f7549912
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-12-15 15:04:32 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
 
-public class Cadastro {
+public class Cadastro extends Pessoa implements Pessoas{
 		public Cadastro(String nome, String email, String senha) {
 			this.nome = nome;
 			this.email = email;
 			this.senha = senha;
 		}
-		public String nome;
-		public String email;
+		
 		public String senha = "senha123";
 		public ArrayList<Filme> filmes = new ArrayList<>();
 		
@@ -16,17 +15,14 @@ public class Cadastro {
 			System.out.println("email:"+email);
 			System.out.println("numero de filmes:"+filmes.size());
 		}
-
-		public String getNome() {
-			return nome;
+		
+		public String obterFuncao() {
+			String f = "Cliente";
+			return f;
 		}
 
 		public void setNome(String nome) {
 			this.nome = nome;
-		}
-
-		public String getEmail() {
-			return email;
 		}
 
 		public void setEmail(String email) {
@@ -47,5 +43,15 @@ public class Cadastro {
 
 		public void setFilmes(ArrayList<Filme> filmes) {
 			this.filmes = filmes;
-		}		
+		}
+
+		@Override
+		public String getNome() {
+			return nome;
+		}
+
+		@Override
+		public String getEmail() {
+			return email;
+		}	
 	}
