20bc00a71a98b05de9fc8b1617b31fa3ca4e82ea
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-12-15 15:07:29 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,38 +1,31 @@
 import java.util.ArrayList;
 
-public class Usuario {
+public class Usuario extends Pessoa implements Pessoas{
 	
 	public Usuario(String nome, String email) {
-		
 		this.nome = nome;
 		this.email = email;
 	}
 	
-	public String nome;
-	public String email;
 	public String senha = "senha123";
 	
 	public ArrayList<Usuario> usuarios = new ArrayList<>();	
 	public ArrayList<Filme> filmes = new ArrayList<>();
 	
 	public void imprimir() {
-		
 		System.out.println("Dados do usuario");
 		System.out.println("Nome:"+nome);
 		System.out.println("Email:"+email);
 		System.out.println("Filmes locados: "+filmes.size());
 	}
-
-	public String getNome() {
-		return nome;
+	
+	public String obterFuncao() {
+		String f = "Cliente";
+		return f;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
 	}
 
 	public void setEmail(String email) {
@@ -62,4 +55,14 @@ public class Usuario {
 	public void setFilmes(ArrayList<Filme> filmes) {
 		this.filmes = filmes;
 	}
+
+	@Override
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String getEmail() {
+		return email;
+	}	
 }
