f6da901c7ad960c770a479b0f0875935d563fdd1
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-12-16 12:28:18 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
+import java.util.ArrayList;//importaçao da biblioteca arraylist
 
+//classe cadastro herdando variaveis e metodo da classe Pessoa e herdando getters e setters da interface Pessoas
 public class Cadastro extends Pessoa implements Pessoas{
 		public Cadastro(String nome, String email, String senha) {
 			this.nome = nome;
@@ -7,20 +8,21 @@ public class Cadastro extends Pessoa implements Pessoas{
 			this.senha = senha;
 		}
 		
-		public String senha = "senha123";
-		public ArrayList<Filme> filmes = new ArrayList<>();
+		public String senha = "senha123";//variavel guardando uma senha pré definida
+		public ArrayList<Filme> filmes = new ArrayList<>();//arryalist de filmes
 		
 		public void imprimir() {
 			System.out.println("nome:"+nome);
 			System.out.println("email:"+email);
 			System.out.println("numero de filmes:"+filmes.size());
-		}
+		}//metodo imprimir da classe cadastro
 		
 		public String obterFuncao() {
 			String f = "Cliente";
 			return f;
-		}
-
+		}//metodo herdado de Pessoa.java
+		
+		//getters e setters
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
@@ -44,7 +46,8 @@ public class Cadastro extends Pessoa implements Pessoas{
 		public void setFilmes(ArrayList<Filme> filmes) {
 			this.filmes = filmes;
 		}
-
+		
+		//getters e setters herdados da interface Pessoas.Java
 		@Override
 		public String getNome() {
 			return nome;
