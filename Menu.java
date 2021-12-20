c36ca4c42b26ca0e2019c00f5c3fb0d6746a99db
Franciny Silva <74836063+francinysilva@users.noreply.github.com>
2021-12-20 02:15:04 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,33 +1,17 @@
-import java.util.Scanner;//importaçao biblioteca scanner
+import java.util.Scanner;
 
 public class Menu {
-
+	//criacao do scanner
 	public static Scanner leitor = new Scanner(System.in);//declaraçao de scanner
-	
-	public static Cadastro lerCadastro() {	
-		System.out.println("Cadastrando um usuario.");
-		System.out.println("Por favor, digite os dados pedidos.");
-		System.out.print("Digite o nome:");		
-		String nome = leitor.nextLine(); 
-		System.out.print("Digite o email:");		
-		String email = leitor.nextLine();
-		System.out.print("Digite o senha:");		
-		String senha = leitor.nextLine();
-		Cadastro cad = null;
-		if (senha.equals(cad.getSenha())) {
-			cad = new Cadastro(nome, email, senha);
-		}
-		return cad;
-	}//metodo pra ler cadastro
-	
-	public static Usuario lerUsuario() {
-		System.out.println("Por favor, digite os dados pedidos.");
-		System.out.print("Digite o nome:");		
-		String nome = leitor.nextLine();
-		System.out.print("Digite o email:");		
-		String email = leitor.nextLine();
-		System.out.print("Digite o senha:");		
-		Usuario usu = new Usuario (nome, email); 
-		return usu; 
+	//mensagens usadas no while do application
+	public static int opcoesLogin() {
+		System.out.println("Digite 1 se já tem cadastro conosco ou digite 2 para se cadastrar");
+		int r = leitor.nextInt();
+		return r;
 	}
-}//metodo ler usuario
+	public static int opcoesLocacao() {
+		System.out.println("Deseja locar qual filme?");
+		int r = leitor.nextInt();
+		return r;
+	}
+}
